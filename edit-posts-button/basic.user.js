6326f9abf4cdbb5fd616039a05cdcf5ef6563ceb
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2022-06-28 17:23:51 +0300

    Reduce console errors

--- edit-posts-button/basic.user.js
+++ edit-posts-button/basic.user.js
@@ -5,27 +5,29 @@
 // @match       *://*
 // @description Add edit post button for own posts (for admin - for all posts) for nodebb forums
 // ==/UserScript==
-async function addEditButtonForPosts () {
-    const [translator] = await app.require(['translator']);
-    const editButtonTitle = await translator.translate('[[topic:edit]]');
-    const posts = $(app.user.isGlobalMod || app.user.isAdmin ? '[component="post"]' : '.self-post').not('.edit-button-was-added');
-    for (const post of posts) {
-        const pid = parseInt($(post).attr('data-pid'), 10);
-        const pHeader = $(post).find('.post-header');
-        const editButton = $('<button>')
-            .addClass('fa fa-fw fa-pencil')
-            .css({ background: 'none', border: 'none' })
-            .tooltip({
-                title: editButtonTitle,
-                placement: 'top',
-                trigger: 'hover'
-            })
-            .click(() => {
-                $(window).trigger('action:composer.post.edit', { pid });
-            });
-        pHeader.append(editButton);
-        $(post).addClass('edit-button-was-added');
+if (typeof $ === 'function') {
+    async function addEditButtonForPosts () {
+        const [translator] = await app.require(['translator']);
+        const editButtonTitle = await translator.translate('[[topic:edit]]');
+        const posts = $(app.user.isGlobalMod || app.user.isAdmin ? '[component="post"]' : '.self-post').not('.edit-button-was-added');
+        for (const post of posts) {
+            const pid = parseInt($(post).attr('data-pid'), 10);
+            const pHeader = $(post).find('.post-header');
+            const editButton = $('<button>')
+                .addClass('fa fa-fw fa-pencil')
+                .css({ background: 'none', border: 'none' })
+                .tooltip({
+                    title: editButtonTitle,
+                    placement: 'top',
+                    trigger: 'hover'
+                })
+                .click(() => {
+                    $(window).trigger('action:composer.post.edit', { pid });
+                });
+            pHeader.append(editButton);
+            $(post).addClass('edit-button-was-added');
+        }
     }
-}
 
-$(window).on('action:posts.loaded action:topic.loaded', addEditButtonForPosts);
+    $(window).on('action:posts.loaded action:topic.loaded', addEditButtonForPosts);
+}
