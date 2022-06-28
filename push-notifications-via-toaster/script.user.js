68379c247a7f67a3d5db72d432cc2e4325082ab6
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2022-06-28 19:34:48 +0300

    Add script: push notification via toaster

---
+++ push-notifications-via-toaster/script.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        push notification via toaster
+// @version     1.0
+// @author      ShlomoCode
+// @match       *://*/*
+// @description push notification incoming via toaster for nodebb forums
+// ==/UserScript==
+/* global socket */
+socket.on('event:new_notification', function (data) {
+    function OpenNotfiPath () {
+        window.open(data.path, '_blank'); // Replace "_blank" with "_ self" for the alert to open on the same tab.
+    }
+    app.alert({
+        title: '[[notifications:new_notification]]',
+        message: data.bodyShort,
+        clickfn: OpenNotfiPath,
+        timeout: 3800,
+        type: 'info'
+    });
+    if (['he', 'ar'].includes(app.user.timeagoCode)) {
+        $('.alert-window').css({ right: 'auto', left: '20px' });
+    }
+});
